9c1f2da21284d0c763e47e1adb14adb46dab2838
tcnksm <nsd22843@gmail.com>
2015-04-01 23:48:56 +0900

    Add httptest

--- json_test.go
+++ json_test.go
@@ -1,9 +1,48 @@
 package latest
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestJSON_implement(t *testing.T) {
 	var _ Source = &JSON{}
+}
+
+func TestJSON_Validate(t *testing.T) {
+	j := &JSON{
+		URL: "http://example.com/info",
+	}
+
+	err := j.Validate()
+	if err != nil {
+		t.Fatalf("expect %s to eq nil", err.Error())
+	}
+}
+
+func TestJSON_Fetch(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `{"version":"1.2.4"}`)
+	}))
+	defer ts.Close()
+
+	j := JSON{
+		URL: ts.URL,
+	}
+
+	versions, malformed, err := j.Fetch()
+	if err != nil {
+		t.Fatalf("expect %s to eq nil", err.Error())
+	}
+
+	if len(malformed) != 0 {
+		t.Fatalf("expect %d to eq 0", len(malformed))
+	}
+
+	expect := "1.2.4"
+	if versions[0].String() != expect {
+		t.Fatalf("expect %s to eq %s", versions[0].String(), expect)
+	}
 }
---
+++ latest_test.go
@@ -0,0 +1,35 @@
+package latest
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestJSON_Check(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `{"version":"1.2.3"}`)
+	}))
+	defer ts.Close()
+
+	j := &JSON{
+		URL: ts.URL,
+	}
+
+	target := "1.2.0"
+	res, err := Check(target, j)
+	if err != nil {
+		t.Fatalf("expect %q to be nil", err.Error())
+	}
+
+	if res.Latest {
+		t.Fatalf("expect %t to be false", res.Latest)
+	}
+
+	expect := "1.2.3"
+	if res.Current != expect {
+		t.Fatalf("expect %q to be %q", res.Current, expect)
+	}
+
+}
