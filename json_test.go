55ba9674d0a450e6c6d18464ca3b21696a8fa139
tcnksm <nsd22843@gmail.com>
2015-04-02 19:46:30 +0900

    Refactoring

--- json_test.go
+++ json_test.go
@@ -1,24 +1,10 @@
 package latest
 
 import (
-	"io"
-	"net/http"
 	"net/http/httptest"
-	"os"
 	"strings"
 	"testing"
 )
-
-func fakeServer(fixture string) *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		f, err := os.Open(fixture)
-		if err != nil {
-			// Should not reach here
-			panic(err)
-		}
-		io.Copy(w, f)
-	}))
-}
 
 func TestJSON_implement(t *testing.T) {
 	var _ Source = &JSON{}
