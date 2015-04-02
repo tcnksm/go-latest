27773c60f55e5befac1615d2b26d2666e51db42e
tcnksm <nsd22843@gmail.com>
2015-04-02 19:46:11 +0900

    Add html source

--- html.go
+++ html.go
@@ -2,27 +2,44 @@ package latest
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
 	"net/url"
+
+	"bytes"
 
 	"github.com/hashicorp/go-version"
 )
 
 type HTML struct {
-	// URL is request URL to fetch version information
-	URL string
-
-	// FixVersionStrFunc transforms version string
-	// so that it can be persed as semantic versioning
-	// by hashicorp/go-version
-	FixVersionStrFunc FixVersionStrFunc
+	URL       string
+	ScrapFunc ScrapFunc
 }
 
-func (h *HTML) fixVersionStrFunc() FixVersionStrFunc {
-	if h.FixVersionStrFunc == nil {
-		return defaultFixVersionStrFunc
+type ScrapFunc func(r io.Reader) string
+
+func ScrapNothing() ScrapFunc {
+	return func(r io.Reader) string {
+		b, _ := ioutil.ReadAll(r)
+		b = bytes.Replace(b, []byte("\n"), []byte(""), -1)
+		return string(b[:])
+	}
+}
+
+var defaultScrapFunc ScrapFunc
+
+func init() {
+	defaultScrapFunc = ScrapNothing()
+}
+
+func (h *HTML) scrapFunc() ScrapFunc {
+	if h.ScrapFunc == nil {
+		return defaultScrapFunc
 	}
 
-	return h.FixVersionStrFunc
+	return h.ScrapFunc
 }
 
 func (h *HTML) Validate() error {
@@ -44,5 +61,49 @@ func (h *HTML) Fetch() ([]*version.Version, []string, error) {
 	var versions []*version.Version
 	var malformed []string
 
+	// URL is validated before call
+	u, _ := url.Parse(h.URL)
+
+	// Create a new request
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return versions, malformed, err
+	}
+	req.Header.Add("Accept", "application/json")
+
+	// Create client
+	t := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		Dial: func(n, a string) (net.Conn, error) {
+			return net.DialTimeout(n, a, defaultDialTimeout)
+		},
+	}
+
+	client := &http.Client{
+		Transport: t,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return versions, malformed, err
+	}
+
+	if resp.StatusCode != 200 {
+		return versions, malformed, fmt.Errorf("unknown status: %d", resp.StatusCode)
+	}
+
+	scrapFunc := h.scrapFunc()
+	verStr := scrapFunc(resp.Body)
+	if len(verStr) == 0 {
+		return versions, malformed, fmt.Errorf("version info is not found on %s", h.URL)
+	}
+
+	v, err := version.NewVersion(verStr)
+	if err != nil {
+		malformed = append(malformed, verStr)
+		return versions, malformed, err
+	}
+
+	versions = append(versions, v)
 	return versions, malformed, nil
 }
--- html_test.go
+++ html_test.go
@@ -1,46 +1,92 @@
 package latest
 
 import (
-	"fmt"
-	"net/http"
+	"io"
 	"net/http/httptest"
 	"testing"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 func TestHTML_implement(t *testing.T) {
 	var _ Source = &HTML{}
 }
 
-func TestHTML_Validate(t *testing.T) {
-	h := &HTML{
-		URL: "http://example.com/info",
+// originalScrap is scrapFunc for test-fixtures/original.html
+// It extracts VERSION from `<div class="version">VERSION</div>`
+func originalScrap(r io.Reader) string {
+
+	// Check function attrs has correct class="version" key&value
+	isTarget := func(targetVal string, attrs []html.Attribute) bool {
+		for _, a := range attrs {
+			if a.Namespace != "" {
+				continue
+			}
+
+			if a.Key == "class" && a.Val == targetVal {
+				return true
+			}
+		}
+		return false
 	}
 
-	err := h.Validate()
-	if err != nil {
-		t.Fatalf("expect %s to eq nil", err.Error())
+	z := html.NewTokenizer(r)
+
+	for {
+		switch z.Next() {
+		case html.ErrorToken:
+			return ""
+		case html.StartTagToken:
+			tok := z.Token()
+			if tok.DataAtom == atom.Div && isTarget("version", tok.Attr) {
+				z.Next()
+				newTok := z.Token()
+				return newTok.String()
+			}
+		}
 	}
 }
 
-func TestHTML_Fetch(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		testHTML := `<html><meta></html>`
-		fmt.Fprintf(w, testHTML)
-	}))
-	defer ts.Close()
-
-	h := HTML{
-		URL: ts.URL,
+func TestHTMLFetch(t *testing.T) {
+	tests := []struct {
+		testServer    *httptest.Server
+		expectCurrent string
+		scrapFunc     ScrapFunc
+	}{
+		{
+			testServer:    fakeServer("test-fixtures/default.html"),
+			expectCurrent: "1.2.3",
+		},
+		{
+			testServer:    fakeServer("test-fixtures/original.html"),
+			expectCurrent: "0.1.2",
+			scrapFunc:     originalScrap,
+		},
 	}
 
-	versions, malformed, err := h.Fetch()
-	if err != nil {
-		t.Fatalf("expect %s to eq nil", err.Error())
+	for i, tt := range tests {
+		ts := tt.testServer
+		defer ts.Close()
+
+		h := &HTML{
+			URL:       ts.URL,
+			ScrapFunc: tt.scrapFunc,
+		}
+
+		versions, _, err := h.Fetch()
+		if err != nil {
+			t.Fatalf("#%d Fetch() expects error:%q to be nil", i, err.Error())
+		}
+
+		if len(versions) == 0 {
+			t.Fatalf("#%d Fetch() expects number of versions found from HTML not to be 0", i)
+		}
+
+		current := versions[0].String()
+		if current != tt.expectCurrent {
+			t.Fatalf("#%d Fetch() expects %s to be %s", i, current, tt.expectCurrent)
+		}
 	}
 
-	if len(malformed) != 0 {
-		t.Fatalf("expect %d to eq 0", len(malformed))
-	}
-
-	_ = versions
 }
