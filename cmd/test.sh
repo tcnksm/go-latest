ac345b5ab1e94dd52fc9b2c72a6d993a38c69892
tcnksm <nsd22843@gmail.com>
2015-04-03 18:17:06 +0900

    Delete cmd

--- cmd/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go build -o latest && \
-    ./latest -repo=ghr -owner=tcnksm -new -debug 0.4.5
-
-echo $? 
-rm ./latest
--- cmd/version.go
+++
@@ -1,5 +0,0 @@
-package main
-
-const Name = "latest"
-
-const Version = "0.1.0"
